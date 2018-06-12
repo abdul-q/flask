90853fd832f813e543f126db6fe0dcb8d9a0f527
abdul-q <ade.abdul@outlook.com>
2018-06-12 10:53:36 +0100

    news flash

---
+++ headlines/headline_route3.py
@@ -0,0 +1,33 @@
+import feedparser
+from flask import Flask
+
+from headlines import app
+
+
+
+news_feeds = {
+"bbc" : "http://feeds.bbci.co.uk/news/rss.xml",
+"dailyTrust" : "https://www.dailytrust.com.ng/rss/feed.aspx?cat_id=3",
+"vanguard_nig" : "https://investornews.vanguard/feed/",
+"sahara" : "http://saharareporters.com/feeds/latest/feed",
+"cnn": "http://rss.cnn.com/rss/edition.rss",
+"fox": "http://feeds.foxnews.com/foxnews/latest",
+"iol": "http://www.iol.co.za/cmlink/1.640"
+}
+
+@app.route('/headlines/<publications>')
+def headlines3(publications="bbc"):
+    feed = feedparser.parse(news_feeds[publications])
+    first_article = feed['entries'] [0]
+    return """<html>
+                    <head>
+                        <title>News Headlines</title>
+                    </head>
+                    <body>
+                        <h2> Vanguard Headlines</h2>
+                        <b>{0}</b> <br />
+                        <i>{1}</i> <br />
+                        <p>{2}</p> <br />
+                    </body>
+            </html>""".format(first_article.get("title"), first_article.get("published"), 
+            first_article.get("summary"))
